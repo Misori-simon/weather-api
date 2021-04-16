bf9c3a58c969d4ee204bb31405fec48c68c61b34
Misori-simon <misorisimon@gmail.com>
2021-04-16 16:57:15 +0100

    Add style to dom elements - Add some css to dom elements - Add some container elements - Add background Image for body

--- src/dom.js
+++ src/dom.js
@@ -5,47 +5,47 @@ const addUnit = (value, unit) => `${value}${unit}`;
 const displayErrorMessage = (cityName) => {
   const resultContainer = document.getElementById('result');
   resultContainer.innerHTML = '';
-  const city = document.createElement('h3');
-  city.classList.add('list-form');
+  const city = document.createElement('p');
+  city.classList.add('error');
   resultContainer.appendChild(city);
-  city.textContent = `Sorry, data for ${cityName} is unavailable`;
+  city.textContent = `Sorry, data for "${cityName}" is unavailable`;
 };
 
 const displayResult = (result, tempUnit) => {
   const resultContainer = document.getElementById('result');
   resultContainer.innerHTML = '';
   const city = document.createElement('h3');
-  city.classList.add('list-form');
+  city.classList.add('city');
   resultContainer.appendChild(city);
   city.textContent = result.city;
 
   const temp = document.createElement('p');
-  temp.classList.add('list-form');
+  temp.classList.add('pill');
   resultContainer.appendChild(temp);
   temp.innerHTML = addUnit(result.temp, `&#176;${tempUnit}`);
 
   const tempRange = document.createElement('p');
-  tempRange.classList.add('list-form');
+  tempRange.classList.add('pill');
   resultContainer.appendChild(tempRange);
   tempRange.innerHTML = addUnit(result.tempRange, `&#176;${tempUnit}`);
 
   const pressure = document.createElement('p');
-  pressure.classList.add('list-form');
+  pressure.classList.add('pill');
   resultContainer.appendChild(pressure);
   pressure.textContent = addUnit(result.pressure, 'hPa');
 
   const humidity = document.createElement('p');
-  humidity.classList.add('list-form');
+  humidity.classList.add('pill');
   resultContainer.appendChild(humidity);
   humidity.textContent = addUnit(result.humidity, '%');
 
   const cloudState = document.createElement('p');
-  cloudState.classList.add('list-form');
+  cloudState.classList.add('pill');
   resultContainer.appendChild(cloudState);
   cloudState.textContent = result.cloudState;
 
   const cloudStateDesc = document.createElement('p');
-  cloudStateDesc.classList.add('list-form');
+  cloudStateDesc.classList.add('pill');
   resultContainer.appendChild(cloudStateDesc);
   cloudStateDesc.textContent = result.cloudStateDesc;
 
@@ -67,33 +67,50 @@ const createForm = (fn, appId) => {
   formContainer.id = ('form-container');
 
   const form = document.createElement('form');
-  form.classList.add('list-form');
+  form.classList.add('form');
   formContainer.appendChild(form);
 
   const city = document.createElement('input');
+  city.classList.add('input-text');
   city.placeholder = 'Enter City';
   form.appendChild(city);
 
+  const tempWraper = document.createElement('div');
+  tempWraper.classList.add('temp-wrapper');
+  form.appendChild(tempWraper);
+
+  const kelvinWraper = document.createElement('span');
+  kelvinWraper.classList.add('temp-unit');
+  tempWraper.appendChild(kelvinWraper);
+
   const tempKelvinLabel = document.createElement('label');
   tempKelvinLabel.textContent = 'Kelvin';
-  form.appendChild(tempKelvinLabel);
+  tempKelvinLabel.setAttribute('for', 'kelvin');
+  kelvinWraper.appendChild(tempKelvinLabel);
 
   const tempKelvin = document.createElement('input');
   tempKelvin.type = 'radio';
   tempKelvin.name = 'temp';
   tempKelvin.value = 'standard';
   tempKelvin.checked = true;
-  form.appendChild(tempKelvin);
+  tempKelvin.id = 'kelvin';
+  kelvinWraper.appendChild(tempKelvin);
+
+  const celciusWrapper = document.createElement('span');
+  celciusWrapper.classList.add('temp-unit');
+  tempWraper.appendChild(celciusWrapper);
 
   const tempCelciusLabel = document.createElement('label');
   tempCelciusLabel.textContent = 'Celcius';
-  form.appendChild(tempCelciusLabel);
+  tempCelciusLabel.setAttribute('for', 'celcius');
+  celciusWrapper.appendChild(tempCelciusLabel);
 
   const tempCelcius = document.createElement('input');
   tempCelcius.type = 'radio';
   tempCelcius.name = 'temp';
   tempCelcius.value = 'metric';
-  form.appendChild(tempCelcius);
+  tempCelcius.id = 'celcius';
+  celciusWrapper.appendChild(tempCelcius);
 
   const tempUnit = () => {
     if (tempCelcius.checked) {
